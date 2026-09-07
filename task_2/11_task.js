db44176b6ec47910bdc148fa696a6c58ee781289
Rudri <Rudri.j@devstree.in>
2026-09-07 11:11:20 +0530

    task doene

--- task_2/11_task.js
+++ task_2/11_task.js
@@ -1,2 +1,18 @@
 
 const temperatures = [0, 15, 30, 5, -10, 25];
+
+// 1 & 2. Convert Celsius to Fahrenheit
+const fahrenheit = temperatures.map(C => (C * 9 / 5) + 32);
+console.log("Fahrenheit:", fahrenheit);
+
+// 3. Temperatures above 20°C
+const above20 = temperatures.filter(C => C > 20);
+console.log( "temp above 20:", above20);
+
+// 4. Highest Celsius temperature
+const highest = Math.max(...temperatures);
+console.log("Highest:", highest);
+
+// 5. Average Celsius temperature
+const average = temperatures.reduce((sum, C) => sum + C, 0) / temperatures.length;
+console.log("Average:", average);
