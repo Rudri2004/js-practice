080a4ed9e4ea2e17cf8ef95ce5bc004b4189ca02
Rudri <Rudri.j@devstree.in>
2026-09-07 15:24:05 +0530

    task done

--- task_2/30_task.js
+++ task_2/30_task.js
@@ -9,19 +9,7 @@ const employees = [
 
 
 // 1. Group employees by department
-const groupedEmployees = employees.reduce((groups, employee) => {
-  const department = employee.department;
 
-  if (!groups[department]) {
-    groups[department] = [];
-  }
-
-  groups[department].push(employee);
-
-  return groups;
-}, {});
-
-console.log("1. Employees grouped by department:", groupedEmployees);
 
 
 // 2. Calculate employee count per department
@@ -56,49 +44,10 @@ console.log("3. Average salary per department:", averageSalary);
 
 
 // 4. Calculate total salary expense per department
-const totalSalaryExpense = Object.entries(groupedEmployees).reduce(
-  (result, [department, employees]) => {
-
-    result[department] = employees.reduce(
-      (total, employee) => total + employee.salary,
-      0
-    );
-
-    return result;
-  },
-  {}
-);
-
-console.log(
-  "4. Total salary expense per department:",
-  totalSalaryExpense
-);
 
 
 // 5. Find the highest-paid employee in every department
-const highestPaidByDepartment = Object.entries(
-  groupedEmployees
-).reduce((result, [department, employees]) => {
-
-  result[department] = employees.reduce(
-    (highest, employee) =>
-      employee.salary > highest.salary ? employee : highest
-  );
-
-  return result;
-}, {});
-
-console.log(
-  "5. Highest-paid employee in every department:",
-  highestPaidByDepartment
-);
 
 
 // 6. Find the department with the highest total salary expense
-const highestExpenseDepartment = Object.entries(
-  totalSalaryExpense
-).reduce((highest, current) =>
-  current[1] > highest[1] ? current : highest
-);
 
-console.log ("6. Department with highest total salary expense:", highestExpenseDepartment);
